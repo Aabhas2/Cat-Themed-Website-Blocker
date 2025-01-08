c448122591d8c4d4bcc00aaa9502c0823c5ea256
Aabhas2 <sharma2.aabhas@gmail.com>
2025-01-08 23:47:47 +0530

    Commited popup.js

---
+++ popup.js
@@ -0,0 +1,57 @@
+const siteInput = document.getElementById("site-input");
+const addBtn = document.getElementById("add-btn");
+const toggleBtn = document.getElementById("toggle-btn");
+const timerInput =document.getElementById("timer-input");
+const timerBtn = document.getElementById("timer-btn");
+const blockedSites = document.getElementById("blocked-sites");
+
+let blockingEnabled = true;
+
+//Add websites to block list
+addBtn.addEventListener("click", () => {
+    const site = siteInput.value.trim();
+    if (site) {
+        chrome.runtime.sendMessage({action: "add"}, (response) => {
+            if (response.status = "added") {
+                updateBlockedSites();
+            }
+        });
+        siteInput.value = "";
+    }
+});
+
+//Toggle blocking 
+toggleBtn.addEventListener('click', () => {
+    blockingEnabled = !blockingEnabled;
+    toggleBtn.textContent = blockingEnabled ? "Turn Off Blocking" : "Turn On Blocking";
+    chrome.runtime.sendMessage({action: blockingEnabled ? "enable" : "disable"});
+});
+
+//Set Timer 
+timerBtn.addEventListener('click', () => {
+    const site = siteInput.value.trim();
+    const duration = parseInt(timerInput.value,10);
+    if(site && duration) {
+        chrome.runtime.sendMessage(
+            {action: "setTimer",site,duration},
+            (response) => {
+                if(response.status === "timer-set") {
+                    updateBlockedSites();
+                }
+            }
+        );
+        siteInput.value = "";
+        timerInput.value = "";
+    }
+});
+
+//Update blocked sites list
+function updateBlockedSites() {
+    chrome.storage.local.get(["blockedSites"],(result) => {
+        const sites = result.blockedSites || [];
+        blockedSitesList.innerHTML = sites.map((site) => `<li>${site}</li>`).join("");
+    });
+}
+
+//initial list laod
+updateBlockedSites();
